b9d0210d687435a6712da5ae1f1b486ff9684fa3
RITHIKRAJESH <rajeshrithik49@gmail.com>
2025-01-30 14:18:27 +0530

    "server changes"

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const dbConnect=require('./models/dbconnect');
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));   
+
+dbConnect()
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
